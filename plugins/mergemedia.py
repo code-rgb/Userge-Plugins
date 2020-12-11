5d0c5ca665194a0e6b10c0a4bb6e98484f5ff96a
Dhruv Maniyaa <71778799+midnightmadwalk@users.noreply.github.com>
2020-12-11 23:45:19 +0530

    mergemedia.py

--- plugins/mergemedia.py
+++ plugins/mergemedia.py
@@ -24,18 +24,18 @@ async def ffmpeg(message: Message):
     x = message.input_str
     if not x:
         await message.edit("`enter valid extension check .help mergemedia`")
-        else:
-            bar = "downloads/output." + x
-            foo = "file 'a." + x + "'" + "\n" + "file 'b." + x + "'"
-            x = open('downloads/merge.txt', 'w+')
-            x.write(foo)
-            x.close()
-            await message.edit("`processing your`"+" "+x+" "+"`files`")
-            await runcmd(f"ffmpeg -f concat -i downloads/merge.txt -map 0 -c copy -scodec copy {bar}")
-            await message.edit("`Uploading`")
-            await upload(message, Path(bar))
-            y = "./downloads"
-            if os.path.exists(y):
-                shutil.rmtree(y)
-                if not os.path.exists(y):
-                    os.mkdir(y)
+    else:
+        bar = "downloads/output." + x.
+        foo = "file 'a." + x + "'" + "\n" + "file 'b." + x + "'"
+        x = open('downloads/merge.txt', 'w+')
+        x.write(foo)
+        x.close()
+        await message.edit("`processing your`"+" "+x+" "+"`files`")
+        await runcmd(f"ffmpeg -f concat -i downloads/merge.txt -map 0 -c copy -scodec copy {bar}")
+        await message.edit("`Uploading`")
+        await upload(message, Path(bar))
+        y = "./downloads"
+        if os.path.exists(y):
+            shutil.rmtree(y)
+            if not os.path.exists(y):
+                os.mkdir(y)
